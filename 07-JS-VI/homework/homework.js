abfba1382c33e071fc68f8dd389da962eff81507
Alejo <ivanrojasantunez@gmail.com>
2021-06-16 19:16:59 -0300

    Finished homework

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,37 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+
+  function takeFisrtLetter(palabra){
+
+    var array = nombre.split("");
+
+    array[0] = array[0].toUpperCase();
+
+    return array.join("");
+
+  }
+
+  return takeFisrtLetter(nombre);
+
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+
+  cb()
+
+
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+
+  return cb(n1, n2)
+
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +42,23 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  
+  var suma = numeros.reduce((acc, el) => acc + el)
+
+  cb(suma)
+ 
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+
+  array.forEach( function(el, i){
+    return cb(el)
+  })
+  
+
 }
 
 function map(array, cb) {
@@ -35,12 +66,31 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+
+  var newArray = [];
+
+  array.forEach((el) => newArray.push(cb(el)))
+
+  return newArray
+  
+
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+
+  function empiezaA (arreglo){
+
+    return arreglo[0] === "a"
+
+  }
+
+  var newArray = array.filter(empiezaA)
+
+  return newArray
+
 }
 
 // No modificar nada debajo de esta línea
